8a1644d683bd29cded8d2c3ad7432f1424b376a3
Joel Cabatay <ascii274@gmail.com>
2022-08-01 20:12:01 +0200

    hello-docker renamed to docker

--- docker/src/main/java/com/ascii274/greeting/DockerApp.java
+++ docker/src/main/java/com/ascii274/greeting/DockerApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DockerGreetingApp {
+public class DockerApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DockerGreetingApp.class, args);
+		SpringApplication.run(DockerApp.class, args);
 	}
 
 }
--- docker/src/test/java/com/ascii274/greeting/DockerAppTests.java
+++ docker/src/test/java/com/ascii274/greeting/DockerAppTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
-class DockerGreetingAppTests {
+class DockerAppTests {
 	@Autowired
 	GreetingController greetingController;
 
